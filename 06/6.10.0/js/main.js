1c4d880e7d1c0d2cb8d1fbd231b12a9ee3227d47
oneGene <jinpark.han@gmail.com>
2018-09-26 08:43:57 +0100

    Project almost done apart from tooltip positioning

--- 06/6.10.0/js/main.js
+++ 06/6.10.0/js/main.js
@@ -16,8 +16,11 @@ var g = svg.append("g")
   .attr("transform", "translate(" + margin.left +
     ", " + margin.top + ")");
 
+let t = () => { return d3.transition().duration(1000); }
+
 // Time parser for x-scale
 var parseTime = d3.timeParse("%d/%m/%Y");
+let formatTime = d3.timeFormat("%d/%m/%Y");
 
 // For tooltip
 var bisectDate = d3.bisector(function(d) { return d.year; }).left;
@@ -28,9 +31,8 @@ var y = d3.scaleLinear().range([height, 0]);
 
 // Axis generators
 var xAxisCall = d3.axisBottom()
+  .ticks(4)
 var yAxisCall = d3.axisLeft()
-  .ticks(6)
-  .tickFormat(function(d) { return parseInt(d / 1000) + "k"; });
 
 // Axis groups
 var xAxis = g.append("g")
@@ -52,11 +54,11 @@ let yAxixLabel = g.append("text")
   .attr("class", "axis-title")
   .attr("transform", "rotate(-90)")
   .attr("x", -height/2)
-  .attr("y", -60)
+  .attr("y", -70)
   .attr("dy", ".71em")
   .attr("font-size", "20px")
-  .style("text-anchor", "end")
-  .attr("fill", "#5D6971")
+  .style("text-anchor", "middle")
+  // .attr("fill", "#5D6971")
 
 
 // Add line to chart
@@ -115,37 +117,90 @@ varSelect.onchange = () => {
   update(dataAll)
 }
 
-// $("#coin-select").on("change", update(dataAll))
+
+// Have to use jquery as it's jquery library
+// let dateSlider = document.getElementById("date-slider")
+// dateSlider.slider({
+//   range: true,
+//   max: parseTime("31/10/2017").getTime(),
+//   min: parseTime("12/5/2013").getTime(),
+//   step: 86400000, // One day
+//   values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
+//   slide: function(event, ui){
+//     $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
+//     $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
+//     update();
+//   }
+// })
+
+// Add jQuery UI slider
+$("#date-slider").slider({
+  range: true,
+  max: parseTime("31/10/2017").getTime(),
+  min: parseTime("12/5/2013").getTime(),
+  step: 86400000, // One day
+  values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
+  slide: function(event, ui){
+    $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
+    $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
+    update(dataAll);
+  }
+});
+
 
 
 function update(data) {
   let coin = document.getElementById("coin-select").value
   let yValue = document.getElementById("var-select").value
 
-  console.log(coin)
-  data = data[coin]
+  let sliderValues = $("#date-slider").slider("values");
+  data = data[coin].filter(function(d) {
+    return ((d.date >= sliderValues[0]) && (d.date <= sliderValues[1]))
+  })
+
+  // console.log(coin)
+  // data = data[coin]
+
+  // console.log(sliderValues)
+  // console.log(data)
+
   // Set scale domains
   x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([d3.min(data, function(d) { return d[yValue]; }) / 1.005,
     d3.max(data, function(d) { return d[yValue]; }) * 1.005]);
 
+  // Fix for format values
+  var formatSi = d3.format(".2s");
+  function formatAbbreviation(x) {
+    var s = formatSi(x);
+    switch (s[s.length - 1]) {
+      case "G": return s.slice(0, -1) + "B";
+      case "k": return s.slice(0, -1) + "K";
+    }
+    return s;
+  }
+
+  // console.log("d[yValue]")
+  // console.log(data[yValue])
+  // console.log("== end d[yValue] ==")
+
   // Generate axes once scales have been set
-  xAxis.call(xAxisCall.scale(x))
-  yAxis.call(yAxisCall.scale(y))
+  xAxisCall.scale(x);
+  xAxis.transition(t()).call(xAxisCall);
+  yAxisCall.scale(y);
+  yAxis.transition(t()).call(yAxisCall.tickFormat(formatAbbreviation));
+// .tickFormat(function(d) { return parseInt(d / 1000) + "k"; });
+// .ticks(6)
 
 
-  // Line path generator
-  var line = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d[yValue]); });
-  console.log(line(data))
+  // xAxis.call(xAxisCall.scale(x))
+  // yAxis.call(yAxisCall.scale(y))
+
+  // Clear old tooltips
+  d3.select(".focus").remove();
+  d3.select(".overlay").remove();
 
 
-  g.select(".line")
-    .attr("d", line(data));
-
-  xAxisLabel.text("hix")
-  yAxixLabel.text("hiy")
 
   /******************************** Tooltip Code ********************************/
 
@@ -185,7 +240,7 @@ function update(data) {
       d1 = data[i],
       d = x0 - d0.date > d1.date - x0 ? d1 : d0;
     focus.attr("transform", "translate(" + x(d.date) + "," + y(d[yValue]) + ")");
-    focus.select("text").text(d[yValue]);
+        focus.select("text").text(function() { return d3.format("$,")(d[yValue].toFixed(2)); });
     focus.select(".x-hover-line").attr("y2", height - y(d[yValue]));
     focus.select(".y-hover-line").attr("x2", -x(d.date));
   }
@@ -193,4 +248,25 @@ function update(data) {
 
   /******************************** Tooltip Code ********************************/
 
+  // Line path generator
+  var line = d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d[yValue]); });
+  // console.log(line(data))
+
+  g.select(".line")
+    .transition(t)
+    .attr("d", line(data));
+
+  // Update axis labels
+  let newText
+  xAxisLabel.text("Time")
+  if (yValue === 'price_usd') {
+    newText = 'Price (USD)'
+  } else if (yValue === 'market_cap') {
+    newText = 'Market Capitalization (USD)'
+  } else {
+    newText = '24 Hour Trading Volume (USD)'
+  }
+  yAxixLabel.text(newText)
 }
